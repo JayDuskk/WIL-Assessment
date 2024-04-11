d9f3340f8ed888d233689a72f53d081f820627da
Joel-Bernoth <99691002+Joel-Bernoth@users.noreply.github.com>
2024-04-11 12:34:09 +1000

    PLayerMovement

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -4,22 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 25.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private float turner;
+    private float looker;
+    public float sensitivity = 5;
 
-    public float speed = 10f;
-
-
-    private Rigidbody rbody;
-
-
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
-        rbody = gameObject.GetComponent<Rigidbody>();    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CharacterController controller = GetComponent<CharacterController>();
+        // is the controller on the ground?
+        if (controller.isGrounded)
+        {
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+            //Jumping
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        turner = Mathf.Clamp(Input.GetAxis("Mouse X") * sensitivity, -90f, 90f);
+        looker = Mathf.Clamp(-Input.GetAxis("Mouse Y") * sensitivity, -90f, 90f);
+        if (turner != 0)
+        {
+            //Code for action on mouse moving right
+            transform.eulerAngles += new Vector3(0, turner, 0);
+        }
+        if (looker != 0)
+        {
+            //Code for action on mouse moving right
+            transform.eulerAngles += new Vector3(looker, 0, 0);
+        }
+        //Applying gravity to the controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        controller.Move(moveDirection * Time.deltaTime);
     }
+
 }
